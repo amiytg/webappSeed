bfca868aed96b622643c66ad73a58d578f0ac691
Amit Gupta <amitg@aditi.com>
2015-10-29 09:42:22 +0530

    seed files

---
+++ index.js
@@ -0,0 +1,24 @@
+﻿require.config({
+    paths: {
+        "bootstrap": "bower_components/bootstrap/dist/js/bootstrap",
+        "jquery": "bower_components/jquery/dist/jquery",
+        "jsnlog": "bower_components/jsnlog/jsnlog",
+        "require-css":"bower_components/require-css/css",
+        "logger":"logger"
+
+    }
+})
+try{
+    require(['logger'],function(logger){
+        require(['jquery'], function (jquery) {    
+            require(['bootstrap'],function(bootstrap){
+                logger.info("Bootstrap loaded")
+            })
+            require(['require-css!css/style.css'], function (style) {
+                logger.info("stylesheet loaded")
+            })
+        })
+    })
+} catch (e) {
+    console.log("Could not initialize app")
+}
---
+++ logger.js
@@ -0,0 +1,12 @@
+﻿define(['jsnlog'], function (jsnlog) {
+    try {
+        var logger = JL();
+        var consoleAppender = JL.createConsoleAppender('consoleAppender');
+        logger.setOptions({ "appenders": [consoleAppender] });
+        logger.info("test")
+    } catch (e) {
+        console.log("Error occured creating logger");
+    }
+    logger.info("Hello")
+    return logger;
+})
