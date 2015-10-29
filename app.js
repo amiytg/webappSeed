79d35dc1e13710c18f4ff46bdb71e4feae837f9b
Amit Gupta <amitg@aditi.com>
2015-10-29 10:40:09 +0530

    supporting angularjs

---
+++ app.js
@@ -0,0 +1,7 @@
+﻿define(['angular','logger'], function (angular,logger) {
+    return angular.module("app", [])
+    .controller("appCtrl", ['$scope', function ($scope) {
+        $scope.title = "Hello"
+        logger.info("Loading controller")
+    }])
+})
